ba0529cd483eea2d31ba8a790b64640b0f1fc0a5
DONOVAN <falcon.donovan@gmail.com>
2018-01-12 18:38:25 -0500

    cambio para integracion con CENTOS7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;//for linux CENTOS 7
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);//for linux CENTOS 7
     }
 
     /**
